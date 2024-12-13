beba6ec0478b7543735b77f4bc5c62aa61317de7
zeblyo <75472200+zeblyo@users.noreply.github.com>
2024-12-13 23:49:40 +0800

    Add files via upload

---
+++ 通用改名法(无视更新).lua
@@ -0,0 +1,4 @@
+local gta5_base = memory.scan("GTA5.exe", "4D 5A")
+memory.write_string(gta5_base + 0x2F25BBF, "RhymeBear")
+--------------GT改为RhymeBear--------------
+--------------daidai改为az3zpz--------------
