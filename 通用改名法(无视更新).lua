8e91fadfe85458b03d2e516321c4040a21e79d47
zeblyo <75472200+zeblyo@users.noreply.github.com>
2024-12-18 19:30:40 +0800

    Update 通用改名法(无视更新).lua

--- 通用改名法(无视更新).lua
+++ 通用改名法(无视更新).lua
@@ -1,4 +1,12 @@
-local gta5_base = memory.scan("GTA5.exe", "4D 5A")
-memory.write_string(gta5_base + 0x2F25BBF, "RhymeBear")
+local memory_name = "RhymeBear"
 --------------GT改为RhymeBear--------------
 --------------daidai改为az3zpz--------------
+
+local gta_addr, socialclub_addr = {0x2A44444, 0x2F22B4C, 0x2F27D8C, 0x2F25BBF}, {0x463B26, 0x4B8467, 0x4C45E8, 0x4C4730, 0x4CD2C8, 0x4CD410, 0x4DA2EC}
+local gta5_base, socialclub_base = memory.scan("GTA5.exe", "4D 5A"), memory.scan("socialclub.dll", "4D 5A")
+local function write_memory(base, addresses)
+    for _, addr in ipairs(addresses) do memory.write_string(base + addr, memory_name) end
+end
+write_memory(gta5_base, gta_addr)
+write_memory(socialclub_base, socialclub_addr)
+util.toast("修改成功")
