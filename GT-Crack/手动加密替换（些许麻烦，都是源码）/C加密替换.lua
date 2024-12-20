5f8b510841b36f923c57d4b8a67ea76ca2fb45fe
zeblyo <75472200+zeblyo@users.noreply.github.com>
2024-12-20 19:25:34 +0800

    Add files via upload

---
+++ GT-Crack/手动加密替换（些许麻烦，都是源码）/C加密替换.lua
@@ -0,0 +1,25 @@
+-- 读取输入文件
+local inputFilePath = filesystem.scripts_dir() .. "C.lua"  -- 请替换为您要加密的文件路径
+local file = assert(io.open(inputFilePath, "r"), "无法打开输入文件")
+local content = file:read("*all")
+file:close()
+
+-- 加密过程
+local function encrypt(text, key)
+    local result = {}
+    local mask = key % 256
+    for i = 1, #text do
+        result[i] = string.char(text:byte(i) ~ mask)
+    end
+    return table.concat(result)
+end
+
+local encrypted = encrypt(content, 7963178524)
+
+-- 将加密结果写入输出文件
+local outputFilePath = filesystem.scripts_dir() .. "lib\\GTLuaVIP\\C.lua"
+local outputFile = assert(io.open(outputFilePath, "w"), "无法创建输出文件")
+outputFile:write(encrypted)
+outputFile:close()
+
+print("加密完成，结果已保存到 " .. outputFilePath)
