1c4703af5a6283e9e7475cf5ff54b9b863c292b1
zeblyo <75472200+zeblyo@users.noreply.github.com>
2024-12-23 16:36:19 +0800

    Update 通用改名法(无视更新).lua

--- 通用改名法(无视更新).lua
+++ 通用改名法(无视更新).lua
@@ -3,12 +3,34 @@ local memory_name = "RhymeBear"
 --------------GT改为RhymeBear--------------
 --------------daidai改为az3zpz--------------
 
-local gta_addr, socialclub_addr = {0x2A44444, 0x2F22B4C, 0x2F27D8C, 0x2F25BBF}, {0x463B26, 0x4B8467, 0x4C45E8, 0x4C4730, 0x4CD2C8, 0x4CD410, 0x4DA2EC}
-local gta5_base, socialclub_base = memory.scan("GTA5.exe", "4D 5A"), memory.scan("socialclub.dll", "4D 5A")
-local function write_memory(base, addresses)
-    for _, addr in ipairs(addresses) do memory.write_string(base + addr, memory_name) end
+local function read_memory(address, offsets)
+    local current_address = address
+    for _, offset in ipairs(offsets) do
+        current_address = memory.read_long(current_address)
+        if current_address == 0 then return nil end
+        current_address = current_address + offset
+    end
+    return current_address
 end
-write_memory(gta5_base, gta_addr)
-write_memory(socialclub_base, socialclub_addr)
+
+local function write_memory(base, addresses)
+    for _, addr in ipairs(addresses) do 
+        memory.write_string(base + addr, memory_name)
+    end
+end
+
+local gta5_base, socialclub_base = memory.scan("GTA5.exe", "4D 5A"), memory.scan("socialclub.dll", "4D 5A")
+
+-- 偏移表
+local offsets = {
+    main = {0x209D810, 0x830, 0x60, 0x2E8, 0xFC},
+    gta = {0x2A44444, 0x2F22B4C, 0x2F27D8C, 0x2F25BBF, 0x203C80B, 0x2F2A244, 0x2F29F3F},
+    socialclub = {0x463B26, 0x4B8467, 0x4C45E8, 0x4C4730, 0x4CD2C8, 0x4CD410, 0x4DA2EC}
+}
+
+memory.write_string(read_memory(gta5_base + offsets.main[1], {table.unpack(offsets.main, 2)}), memory_name)
+write_memory(gta5_base, offsets.gta)
+write_memory(socialclub_base, offsets.socialclub)
 memory.write_long(gta5_base + 0x2F27CC0, 227946978)
+
 util.toast("修改成功")
